f0acdcb08b920153ecb48de50b69060c7d8ba94a
xiaowei <shenzhoudance@gmail.com>
2018-04-01 11:23:56 +0800

    add comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331055004) do
+ActiveRecord::Schema.define(version: 20180401021635) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "shot_id"
+  end
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
